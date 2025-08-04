ed9cda97fe7afe598e32fa4b751a954698a3c5e6
Farseer19 <90807471+Farseer19@users.noreply.github.com>
2025-08-03 21:06:18 -0400

    Update Project Code.py
    Cleaned up comments

--- Project Code.py
+++ Project Code.py
@@ -3,29 +3,29 @@ f"Algorithm to check each reading frame for start and stop codons, storing their
 
 f"Parse input FASTA files that may have errors  "
 
-#Author: Duncan Hall
-#Function for handling input of FASTA file
+# Author: Duncan Hall
+# Function for handling input of FASTA file
 def input_FASTA ():
     f"Dictionary that stores header information and corresponding full sequences in all caps"
     return_FASTA = dict()
-    #Opening stored FASTA file
+    # Opening stored FASTA file
     with open(r"input.txt") as f:
         f"Sets current FASTA entry where sequence data is being added to"
         curr_FASTA = str()
-        #Parse through each line in file
+        # Parse through each line in file
         for line in f:
-            #Strip each line in file
+            # Strip each line in file
             line=line.strip()
-            #Checks if there is anything in this "line"
+            # Checks if there is anything in this "line"
             if line:
-                #If the line is the header, the key in the dictionary will be set that with a string as the value to be extended as sequence lines are read
+                # If the line is the header, the key in the dictionary will be set that with a string as the value to be extended as sequence lines are read
                 if line[0] == '>':
                     curr_FASTA = line
                     return_FASTA[curr_FASTA] = str()
-                #Sequence is extended while there is a line and that line is not a header
+                # Sequence is extended while there is a line and that line is not a header
                 else:
                     return_FASTA[curr_FASTA] += line.upper()
-    #Return the FASTA dictionary
+    # Return the FASTA dictionary
     return return_FASTA
 
 
@@ -44,7 +44,7 @@ def reverse_complement(seq):
 def find_orfs_in_frame(seq, min_len, reverse):
     # List to store found ORFs
     orfs = []
-
+    # Checks if this is a reverse complement call and changes the sequence to the reverse complement
     if reverse:
         seq = reverse_complement(seq)
     # Loop through all three frame offsets (0, 1, or 2)
@@ -104,25 +104,27 @@ def find_orfs_in_frame(seq, min_len, reverse):
     # Return list of ORFs found in this frame
     return orfs
 
-#Author: Duncan Hall
-#Function to call input_FASTA function and find_orfs_in_frame in conjunction to create the output. Only variable is min_len so the desired minimum length can be passed to the find_orfs_in_frame call.
+# Author: Duncan Hall
+# Function to call input_FASTA function and find_orfs_in_frame in conjunction to create the output. Only variable is min_len so the desired minimum length can be passed to the find_orfs_in_frame call.
 def orfs_from_input(min_len):
-    #Call input_FASTA to retrieve sequence data
+    # Call input_FASTA to retrieve sequence data
     input_dict = input_FASTA()
-    #Create output_dict to store output data
+    # Create output_dict to store output data
     output_dict = {}
     f"Look through each entry from the input_FASTA function and process to find ORFs"
     for entry in input_dict:
-        #Calls find_orfs_in_frame in the forward direction and stores the data in output_dict
+        # Calls find_orfs_in_frame in the forward direction and stores the data in output_dict
         output_dict[entry] = find_orfs_in_frame(input_dict[entry],min_len,True)
-        #Calls find_orfs_in_frame in the reverse direction and stores the data in output_dict
+        # Calls find_orfs_in_frame in the reverse direction and stores the data in output_dict
         output_dict[entry] += find_orfs_in_frame(input_dict[entry],min_len,False)
     
     print(output_dict)
     f"Print out in viewable format"
     for entry in output_dict:
         for orf in output_dict[entry]:
+            # Prints out Frame, Position and Length information
             print(entry, "|FRAME=",orf[0],"POS=",orf[1],"LEN=",len(orf[2]))
+            # Creates string of space separated codons from the sequence
             output_seq = " ".join([orf[2][i:i+3] for i in range(0, len(orf[2]), 3)])
             print(output_seq)
 
